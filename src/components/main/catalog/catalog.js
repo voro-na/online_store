6b1b0bbe3e0baf74175d36a6e3e9230eaf478366
voro-na <voropaevaanadya@gmail.com>
2022-10-14 15:42:34 +0300

    add img fix cards layout

--- src/components/main/catalog/catalog.js
+++ src/components/main/catalog/catalog.js
@@ -1,85 +1,95 @@
 import styles from './catalog.module.css'
 
+
 const catalog = () => {
     const cart = {
         headphones: [
             {
+                img: 'img/1.svg',
                 title: 'Apple BYZ S852I',
                 price: 2927,
                 rate: 4.7,
             },
             {
+                img: 'img/2.svg',
                 title: 'Apple EarPods',
                 price: 2327,
                 rate: 4.5,
             },
             {
+                img: 'img/3.svg',
                 title: 'Apple EarPods',
                 price: 2327,
                 rate: 4.5,
             },
             {
+                img: 'img/1.svg',
                 title: 'Apple BYZ S852I',
                 price: 2927,
                 rate: 4.7,
             },
             {
-                title: 'Apple EarPods',
-                price: 2327,
-                rate: 4.5,
+                img: 'img/2.svg',
+                title: 'Apple BYZ S852I',
+                price: 2927,
+                rate: 4.7,
             },
             {
+                img: 'img/3.svg',
                 title: 'Apple EarPods',
                 price: 2327,
                 rate: 4.5,
             },
         ],
-        wirelessHeadphones:[
+        wirelessHeadphones: [
             {
+                img: 'img/4.svg',
                 title: 'Apple AirPods',
                 price: 9527,
                 rate: 4.7,
             },
             {
+                img: 'img/5.svg',
                 title: 'GERLAX GH-04',
                 price: 6527,
                 rate: 4.7,
             },
             {
+                img: 'img/6.svg',
                 title: 'BOROFONE BO4',
                 price: 7527,
                 rate: 4.7,
             }
         ]
     }
-    const result=[]
-    for (let key in cart){
-        const temp = cart[key].map((product)=>{
-            return(
-                <div className={styles.container}>
-                    <div>
+    const result = []
+    for (let key in cart) {
+        const temp = cart[key].map((product) => {
+            return (
+                <div className={styles.cardContainer}>
+                    <img src={`${product.img}`}/>
+
+                    <div className={styles.title}>
                         <div>{product.title}</div>
-                        <div>{product.price}</div>
+                        <div style={{color: '#FFA542'}}>{product.price} ₽</div>
                     </div>
-                    <div>
-                        <div>{product.rate}</div>
+
+                    <div className={styles.rate}>
+                        <div className='_icon-star'></div>
+                        <div style={{color: '#838383'}}>{product.rate}</div>
                         <div>Купить</div>
                     </div>
 
                 </div>
             )
         })
-
         result.push(temp)
     }
-    console.log(result)
-    const final = result.map((t)=><div>{t}</div>)
+    const final = result.map((t) => t)
 
     return (
-
-        <div>
+        <div className={styles.container}>
             {final}
-            catalog
         </div>
     )
 }
